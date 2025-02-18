835061fafc39999eaa8daa6c51f6de3dd0c0f110
rohitgajbhiye744 <rohitgajbhiye072@gmail.com>
2025-02-18 19:47:04 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext';
+import { StrictMode } from 'react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
